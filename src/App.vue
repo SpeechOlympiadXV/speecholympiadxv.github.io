2f0a28d5300cff604f7944e9a11a12e867039c5e
Sandil Ranasinghe <sandilsranasinghe@gmail.com>
2022-08-09 00:23:21 +0530

    init project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Sample from "./components/Sample.vue";
+</script>
+
+<template>
+  <div>
+    <Sample />
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Sample.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="heading">
+    Welcome to Speech Olympiad Site! Under Construction :(
+  </div>
+</template>
+
+<style scoped>
+.heading {
+  font-size: 2.5rem;
+}
+</style>
