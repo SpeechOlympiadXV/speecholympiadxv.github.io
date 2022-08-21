7f7f8e0ff45c48b916f340a88c2e581f466c4d32
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2022-08-21 14:46:22 +0530

    started working on footer

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,30 @@
+<script>
+import SOLogo from './SOLogo.vue'
+
+export default {
+  components :{
+    SOLogo
+  },
+  data(){
+    return {
+
+    }
+  }
+}
+</script>
+
+
+<template>
+    <div class="container w-100">
+        <div class="row">
+          <div class="col-12 col-md-8">
+            <SOLogo />
+          </div>
+          <div class="col-12 col-md-4">
+            <div>Topic</div>
+          </div>
+        </div>
+        <div class="footer-links"></div>
+    </div>
+</template>
+
