ce5828ae17350245a47b073532d2f49d6fb5f601
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2022-09-18 19:15:06 +0530

    updated SO Logo

--- src/components/SOLogo.vue
+++ src/components/SOLogo.vue
@@ -1,5 +1,5 @@
 <script>
-import SOLogo from '../assets/images/so-logo.png'
+import SOLogo from '../assets/images/SOXIV_LOGO_White-small.png'
 
 export default {
   data() {
@@ -14,8 +14,11 @@ export default {
 </script>
 
 <template>
-  <img :src="logo.filename" :alt="logo.alt">
+  <img :src="logo.filename" :alt="logo.alt" class="logo">
 </template>
 
 <style scoped>
+.logo{
+  object-fit: cover;
+}
 </style>
