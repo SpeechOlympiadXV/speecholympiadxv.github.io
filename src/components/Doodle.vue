2c94118408a1e9cd9b7b70bd18c1a388229415b2
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2022-09-20 00:35:57 +0530

    updated doodle svg

--- src/components/Doodle.vue
+++ src/components/Doodle.vue
@@ -1,60 +1,52 @@
 <template>
   <div id="doodle">
-    <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 331.14 537.57">
-      <path
-          d="M384,169.89S320.13,179,241,252c0,0-18.38,12.76-33.19,14.3,0,0-22.47,40.85-51.07,47.48,0,0-15.83,258.9,88.34,388.09H485V180.53S430,157.72,384,169.89Z"
-          transform="translate(-154.86 -165.32)" fill="none" stroke="#000" stroke-miterlimit="10" stroke-width="2"/>
-      <path
-          d="M254.77,701.89c-105.71-155.74-89.88-381.45-89.88-381.45,33.2-16.34,46.47-43.91,46.47-43.91s21.45-2.55,71.49-48S406.43,180,406.43,180s49.91-2.56,78.57,18.89"
-          transform="translate(-154.86 -165.32)" fill="none" stroke="#000" stroke-miterlimit="10" stroke-width="2"/>
-      <path
-          d="M485,212.7s-83.9-54.39-184.28,12.76c0,0-3.57,3.07-55.66,45.45,0,0-19.33,14.5-29.1,14.3,0,0-24,36.24-41.87,39.32,0,0-6.8,218.23,74.08,343.81a231.72,231.72,0,0,0,26,33.55"
-          transform="translate(-154.86 -165.32)" fill="none" stroke="#000" stroke-miterlimit="10" stroke-width="2"/>
-      <path d="M248.17,668.34c49.57,31.31,97.49-14.45,97.49-14.45s13.28,43.91,107.23,48"
-            transform="translate(-154.86 -165.32)" fill="none" stroke="#000" stroke-miterlimit="10" stroke-width="2"/>
-      <path
-          d="M467.7,406.23c-8.17,60.77-56.17,95.5-114.38,94.47-33-.58-80.66-39-74-99.58,6.63-60.76,53.61-99.57,106.21-99.57S475.82,345.86,467.7,406.23Z"
-          transform="translate(-154.86 -165.32)" fill="none" stroke="#000" stroke-miterlimit="10" stroke-width="2"/>
-      <path
-          d="M462.76,405.52c-7.74,57.64-53.23,90.59-108.38,89.61-31.3-.55-76.43-37-70.16-94.46,6.29-57.64,50.8-94.46,100.64-94.46S470.45,348.24,462.76,405.52Z"
-          transform="translate(-154.86 -165.32)" fill="none" stroke="#000" stroke-miterlimit="10" stroke-width="2"/>
-      <path
-          d="M438.7,404.73c-5.95,42.84-40.87,67.33-83.21,66.6-24-.41-58.67-27.51-53.86-70.2,4.83-42.85,39-70.21,77.26-70.21S444.6,362.16,438.7,404.73Z"
-          transform="translate(-154.86 -165.32)" fill="none" stroke="#000" stroke-miterlimit="10" stroke-width="2"/>
-      <path
-          d="M408.44,415.54c-14.87,40.33-40.79,58.48-64,51.21-13.17-4.12-25-35.59-10.79-75.95s40.54-61.54,61.69-55.6S423.22,375.48,408.44,415.54Z"
-          transform="translate(-154.86 -165.32)" fill="none" stroke="#000" stroke-miterlimit="10" stroke-width="2"/>
-      <path
-          d="M387.3,409.3c-13,40.31-28.52,61.07-39.24,57.31-6.09-2.13-7.78-30.41,4.86-70.66s28.81-63.94,38.67-61.17S400.19,369.25,387.3,409.3Z"
-          transform="translate(-154.86 -165.32)" fill="none" stroke="#000" stroke-miterlimit="10" stroke-width="2"/>
-      <path
-          d="M365.15,412.61c-35.41-10.13-54-22.4-51.26-30.95,1.58-4.86,25.95-6.34,61.29,3.53s56.53,22.61,54.57,30.48S400.33,422.68,365.15,412.61Z"
-          transform="translate(-154.86 -165.32)" fill="none" stroke="#000" stroke-miterlimit="10" stroke-width="2"/>
-      <path
-          d="M356.49,444.75c-31.82-9.11-48.55-20.19-46-27.94,1.43-4.39,23.34-5.78,55.1,3.09s50.79,20.39,49,27.51S388.1,453.81,356.49,444.75Z"
-          transform="translate(-154.86 -165.32)" fill="none" stroke="#000" stroke-miterlimit="10" stroke-width="2"/>
-      <path
-          d="M375.75,379.46c-31.82-9.12-45.37-18.08-42.86-25.83,1.43-4.39,20.16-7.9,51.93,1s50.78,20.38,49,27.5S407.37,388.51,375.75,379.46Z"
-          transform="translate(-154.86 -165.32)" fill="none" stroke="#000" stroke-miterlimit="10" stroke-width="2"/>
-      <path
-          d="M388.5,350.11c-15.09-3.95-22.91-9.95-21.59-14.93.76-2.82,11.27-4.89,26.34-1.09s24,10,23,14.49S403.49,354,388.5,350.11Z"
-          transform="translate(-154.86 -165.32)" fill="none" stroke="#000" stroke-miterlimit="10" stroke-width="2"/>
-      <path d="M383.19,332s-51.06,3.23-65.62,41.53c0,0-34.79,63.84,29.19,93.84" transform="translate(-154.86 -165.32)"
-            fill="none" stroke="#000" stroke-miterlimit="10" stroke-width="2"/>
-      <polygon points="66.71 144.41 149.43 138.79 146.32 161.77 66.71 166.7 66.71 144.41" fill="none" stroke="#000"
-               stroke-miterlimit="10" stroke-width="2"/>
-      <polygon points="44.25 182.08 127.03 177.52 123.62 200.45 43.96 204.37 44.25 182.08" fill="none" stroke="#000"
-               stroke-miterlimit="10" stroke-width="2"/>
-      <polygon points="30.78 217.12 113.57 212.56 111.73 238.36 30.49 239.41 30.78 217.12" fill="none" stroke="#000"
-               stroke-miterlimit="10" stroke-width="2"/>
-      <polygon points="28.88 251.52 111.73 248.25 109.86 273.81 28.24 273.81 28.88 251.52" fill="none" stroke="#000"
-               stroke-miterlimit="10" stroke-width="2"/>
-      <polygon points="37.64 289.59 120.53 287.49 119.31 314.45 36.69 311.87 37.64 289.59" fill="none" stroke="#000"
-               stroke-miterlimit="10" stroke-width="2"/>
-      <polygon points="44.62 325.55 127.41 325.55 124.92 353.26 43.58 349.93 44.62 325.55" fill="none" stroke="#000"
-               stroke-miterlimit="10" stroke-width="2"/>
-      <polyline points="330.14 294.53 308.94 294.53 307.22 320.57 330.14 321.6" fill="none" stroke="#000"
-                stroke-miterlimit="10" stroke-width="2"/>
+    <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 331.13 537.58">
+      <g id="Layer_1-2" data-name="Layer 1">
+        <path
+            d="M384,169.89S320.13,179,241,252c0,0-18.38,12.77-33.19,14.3,0,0-22.47,40.85-51.06,47.49,0,0-15.83,258.89,88.34,388.09H485V180.53S430,157.72,384,169.89Z"
+            transform="translate(-154.87 -165.32)" fill="none" stroke="#000" stroke-miterlimit="10" stroke-width="2"/>
+        <path
+            d="M254.77,701.89C149.07,546.15,164.9,320.44,164.9,320.44c33.19-16.34,46.47-43.91,46.47-43.91s21.45-2.55,71.49-48S406.43,180,406.43,180s49.91-2.55,78.57,18.89"
+            transform="translate(-154.87 -165.32)" fill="none" stroke="#000" stroke-miterlimit="10" stroke-width="2"/>
+        <path
+            d="M485,212.7s-83.9-54.39-184.28,12.77c0,0-3.57,3.06-55.66,45.45,0,0-19.34,14.5-29.11,14.3,0,0-24,36.24-41.87,39.32,0,0-6.79,218.23,74.08,343.82a232.26,232.26,0,0,0,26,33.55"
+            transform="translate(-154.87 -165.32)" fill="none" stroke="#000" stroke-miterlimit="10" stroke-width="2"/>
+        <path d="M248.17,668.34c49.57,31.31,97.49-14.45,97.49-14.45s13.28,43.91,107.23,48"
+              transform="translate(-154.87 -165.32)" fill="none" stroke="#000" stroke-miterlimit="10" stroke-width="2"/>
+        <path
+            d="M467.7,406.23c-8.17,60.77-56.18,95.5-114.38,94.47-33-.58-80.66-39-74-99.57,6.64-60.77,53.62-99.57,106.21-99.57S475.82,345.86,467.7,406.23Z"
+            transform="translate(-154.87 -165.32)" fill="none" stroke="#000" stroke-miterlimit="10" stroke-width="2"/>
+        <path
+            d="M462.76,405.52c-7.74,57.65-53.23,90.59-108.38,89.62-31.29-.55-76.43-37-70.16-94.46,6.29-57.65,50.8-94.46,100.64-94.46S470.45,348.24,462.76,405.52Z"
+            transform="translate(-154.87 -165.32)" fill="none" stroke="#000" stroke-miterlimit="10" stroke-width="2"/>
+        <path
+            d="M438.7,404.73c-5.94,42.84-40.86,67.33-83.2,66.61-24-.41-58.67-27.51-53.86-70.21s39-70.21,77.26-70.21S444.6,362.16,438.7,404.73Z"
+            transform="translate(-154.87 -165.32)" fill="none" stroke="#000" stroke-miterlimit="10" stroke-width="2"/>
+        <path
+            d="M408.44,415.54c-14.87,40.33-40.79,58.48-64,51.21-13.17-4.13-25-35.59-10.78-75.95s40.54-61.54,61.69-55.6S423.22,375.48,408.44,415.54Z"
+            transform="translate(-154.87 -165.32)" fill="none" stroke="#000" stroke-miterlimit="10" stroke-width="2"/>
+        <path
+            d="M387.3,409.3c-13,40.31-28.52,61.07-39.24,57.31-6.08-2.13-7.78-30.41,4.87-70.66S381.74,332,391.6,334.78,400.19,369.25,387.3,409.3Z"
+            transform="translate(-154.87 -165.32)" fill="none" stroke="#000" stroke-miterlimit="10" stroke-width="2"/>
+        <path
+            d="M365.15,412.61c-35.41-10.13-54-22.4-51.26-30.95,1.57-4.85,25.94-6.34,61.29,3.53s56.53,22.62,54.57,30.48S400.33,422.68,365.15,412.61Z"
+            transform="translate(-154.87 -165.32)" fill="none" stroke="#000" stroke-miterlimit="10" stroke-width="2"/>
+        <path
+            d="M356.49,444.75c-31.82-9.11-48.54-20.19-46-27.94,1.42-4.4,23.34-5.78,55.1,3.09s50.78,20.38,49,27.51S388.1,453.81,356.49,444.75Z"
+            transform="translate(-154.87 -165.32)" fill="none" stroke="#000" stroke-miterlimit="10" stroke-width="2"/>
+        <path
+            d="M375.75,379.46c-31.82-9.11-45.37-18.07-42.86-25.82,1.42-4.4,20.16-7.9,51.92,1s50.78,20.38,49,27.51S407.37,388.51,375.75,379.46Z"
+            transform="translate(-154.87 -165.32)" fill="none" stroke="#000" stroke-miterlimit="10" stroke-width="2"/>
+        <path
+            d="M388.5,350.11c-15.09-3.95-22.92-9.95-21.59-14.93.75-2.82,11.27-4.89,26.33-1.09s24,10,23,14.5S403.49,354,388.5,350.11Z"
+            transform="translate(-154.87 -165.32)" fill="none" stroke="#000" stroke-miterlimit="10" stroke-width="2"/>
+        <path d="M383.19,332s-51.06,3.24-65.62,41.53c0,0-34.8,63.84,29.19,93.84" transform="translate(-154.87 -165.32)"
+              fill="none" stroke="#000" stroke-miterlimit="10" stroke-width="2"/>
+        <path d="M485,486.91" transform="translate(-154.87 -165.32)" fill="none" stroke="#000" stroke-miterlimit="10"
+              stroke-width="2"/>
+        <path d="M485,459.85" transform="translate(-154.87 -165.32)" fill="none" stroke="#000" stroke-miterlimit="10"
+              stroke-width="2"/>
+      </g>
     </svg>
   </div>
 </template>
@@ -112,7 +104,7 @@ let doodle = {
       // Make very long dashes (the length of the path itself)
       path.style.strokeDasharray = pathLength;
 
-      // Offset the dashes so the it appears hidden entirely
+      // Offset the dashes so it appears hidden entirely
       path.style.strokeDashoffset = pathLength;
 
       // When the page scrolls...
