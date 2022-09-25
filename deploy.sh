84588896817bdf85dd60dfd79900c0d942ccdcc5
Sandil Ranasinghe <sandilsranasinghe@gmail.com>
2022-09-25 20:31:18 +0530

    correct branch name in force deploy

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ git commit -m "new deployment"
 
 # git subtree push --prefix dist origin gh-pages
 
-git push origin `git subtree split --prefix dist master`:gh-pages --force
+git push origin `git subtree split --prefix dist deploy/1.0`:gh-pages --force
