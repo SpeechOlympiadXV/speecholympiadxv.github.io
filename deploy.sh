3ced999958edbd3bbb47c600212171f8027d33b0
Sandil Ranasinghe <sandilsranasinghe@gmail.com>
2022-09-27 18:36:22 +0530

    change deplo command

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ git add dist
 
 git commit -m "new deployment"
 
-git subtree push --prefix dist origin gh-pages
+# git subtree push --prefix dist origin gh-pages
 
-#git push origin `git subtree split --prefix dist master`:gh-pages --force
+git push origin `git subtree split --prefix dist deploy/1.1.1`:gh-pages --force
