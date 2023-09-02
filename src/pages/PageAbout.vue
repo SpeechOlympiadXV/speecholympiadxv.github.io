d2b90f6bfacafe02773417bd1bdf350a3c3dc017
Savinu <howfeels189@gmail.com>
2023-09-02 19:16:01 +0530

    about changed

--- src/pages/PageAbout.vue
+++ src/pages/PageAbout.vue
@@ -32,27 +32,33 @@ export default {
     </AppHeroSection>
     <AppArticle>
       <p class="mt-3 first-letter">
-        Speech Olympiad stands as the flagship occasion hosted by the Gavel Club at the University of Moratuwa. As the
-        sole English language speech competition within the university, it garners participants from all five faculties.
-        Since its establishment in 2005, this event has progressively gained momentum, evolving into one of the most
-        highly anticipated highlights on the university calendar. In an era where effective communication is paramount in
-        any industry, Speech Olympiad championship has become a coveted goal pursued by all young undergraduates. The
-        enthusiasm instilled by faculty members to engage their students in this competition has led to a consistent rise
-        in participation from all academic departments. While the Speech Olympiad certainly boasts prestige, it transcends
-        mere title-seeking; it provides a platform for personal growth and the potential to ignite transformative change
+        Speech Olympiad stands as the flagship occasion hosted by the Gavel Club of University of Moratuwa. 
+        As the sole English language speech competition within the university, it garners participants from all 
+        five faculties. Since its establishment in 2005, this event has progressively gained momentum, evolving 
+        into one of the most highly anticipated phenomenon on the university calendar. In an era where effective 
+        communication is paramount in any industry, The Speech Olympiad championship has become a coveted goal 
+        pursued by all young undergraduates. The enthusiasm instilled by faculty members to engage their students 
+        in this competition has led to a consistent rise in participation from all academic departments.
+        While the Speech Olympiad championship certainly holds prestige, it transcends above a title. 
+        It provides a platform for personal growth and the potential to ignite transformative change 
         not only within oneself but also among peers.
       </p>
-
+      
+      <p>
+        <br/>
+      </p>
 
       <p>
-        Throughout the span of the last 15 Speech Olympiad editions, there has been a consistent demonstration of the
-        event’s ability to unveil hidden talents within the university’s undergraduate population. The recipients of
-        awards and the participants who reach the final stages encompass a broad spectrum of backgrounds, highlighting the
-        event’s widespread appeal that transcends various academic disciplines. Countless champions and finalists not only
-        in the Inter-University Best Speaker Contest and All Island Best Speaker Contest but also in several other
-        esteemed All Island and Inter-University Competitions such as Viva Voce, Eloquence, Speech Weaver, Templanza, and
-        Enliven, have emerged as winners, attributing their recognition to their involvement in preceding Speech Olympiad
-        competitions.
+
+  
+        Throughout the span of the last 15 years, there has been a 
+        consistent demonstration of the event’s ability to unveil hidden 
+        talents within the university’s undergraduates. The recipients of awards 
+        and those who reach the final stages encompass a broad spectrum of backgrounds, 
+        highlighting the event’s widespread appeal through all academic disciplines. Countless  
+        participants of Speech Olympiad have then proceeded to dominate the National and International 
+        stages throughout the past decade, emerging as well-rounded impactful speakers who touch the hearts of many.
+
       </p>
 
       <!-- <p>
